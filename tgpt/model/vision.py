111a1967bb330c98d035633902003a66134cebd7
yeoshuheng <yeoshuheng@gmail.com>
2024-03-02 01:41:17 +0800

    feat: Add yolov3 for traffic tracking

--- tgpt/model/vision.py
+++ tgpt/model/vision.py
@@ -24,3 +24,55 @@ class TrafficTracker:
             cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
             ret += 1
         return ret, Image.fromarray(img)
+  
+class TrafficTrackerYoloV3:
+    def __init__(self, confidence=0.5):
+        self.loader = ImagePipeline()
+        self.threshold=confidence
+        dir = os.path.dirname(__file__)
+        weights_path = os.path.join(dir, "resources/yolov3.weights")
+        cfg_path = os.path.join(dir, "resources/yolov3.cfg")
+        names_path = os.path.join(dir, "resources/coco.names")
+        self.model = cv2.dnn.readNet(model=weights_path, config=cfg_path)
+        with open(names_path, "r") as f:
+            self.classes = [line.strip() for line in f.readlines()]
+        layer_names = self.model.getLayerNames()
+        self.colors = np.random.uniform(0, 255, size=(len(self.classes), 3))
+        self.output_layers = [layer_names[i - 1] for i in self.model.getUnconnectedOutLayers()]
+
+    def get_count(self):
+        img = self.loader.get_data_as_user()
+        img = np.array(img)
+        img = cv2.resize(img, None, fx=0.4, fy=0.4)
+        h, w, c = img.shape
+        blob = cv2.dnn.blobFromImage(img, scalefactor=0.00392, 
+                                     size=(320, 320), mean=(0, 0, 0), 
+                                     swapRB=True, crop=False)
+        self.model.setInput(blob)
+        out = self.model.forward(self.output_layers)
+        bev_bounds = []
+        confs = []
+        ids = []
+        for output in out:
+            for det in output:
+                scores = det[5:]
+                id_curr = np.argmax(scores)
+                conf = scores[id_curr]
+                if conf >= self.threshold:
+                    cx, cy, width, height = [int(a*b) for a, b in zip(det[0 : 4], [w, h, w, h])]
+                    x, y = int(cx - (width/2)), int(cy - (height/2))
+                    ids.append(id_curr)
+                    confs.append(conf)
+                    bev_bounds.append([x, y, width, height])
+        indices = cv2.dnn.NMSBoxes(bev_bounds, confs, self.threshold, 0.5)
+        indices = indices.flatten()
+        count = 0
+        for i in indices:
+            (x, y, w, h) = bev_bounds[i][0], bev_bounds[i][1], bev_bounds[i][2], bev_bounds[i][3]
+            color = self.colors[i]
+            cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
+            text = f"{ids[i]}: {confs[i] * 100:.2f}%"
+            cv2.putText(img, text, (x, y - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, 2)
+            if ids[i] in [2, 3, 5, 6, 7]:
+                count += 1
+        return count, Image.fromarray(img)
