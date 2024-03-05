4aab9f21c3959f1698c86b2a7b8753a0e44f9a84
jdengoh <spect13317@gmail.com>
2024-03-05 18:15:16 +0800

    edited trafficFlow - json link that leads to output

--- tgpt/data/trafficFlow.py
+++ tgpt/data/trafficFlow.py
@@ -4,23 +4,27 @@ import pathos.multiprocessing as p
 
 class TrafficFlowPipeline:
     def __init__(self):
-        self.url = "http://datamall2.mytransport.sg/ltaodataservice/trafficflow"
+        self.url = "http://datamall2.mytransport.sg/ltaodataservice/TrafficFlow"
         self.headers = {'AccountKey': os.environ.get("LTA_KEY"), "accept": "application/json"}
         # self.n_core = os.cpu_count() // 2
 
     def get_traffic_data(self):
         try:
             response = requests.get(self.url, headers=self.headers)
-            if response.status_code == 200:
-                return response.json()
-            else:
-                return None
+            response.raise_for_status()
+            print("success")
+            return response.json()
+            # if response.status_code == 200:
+            #     return response.json()
+            # else:
+            #     return None
         except requests.RequestException as e:
             print(f"Error fetching data: {e}")
             return None
 
 trying = TrafficFlowPipeline()
 data = trying.get_traffic_data()
+
 if data is not None:
     for i in data['value']:
         print(i)
