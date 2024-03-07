8647e268def69584f5904b34bde6313d77fcbcc2
jdengoh <spect13317@gmail.com>
2024-03-08 03:31:07 +0800

    working on function to process the data of start and end points automatically:

--- tgpt/data/EstTravelTimes.py
+++ tgpt/data/EstTravelTimes.py
@@ -1,8 +1,11 @@
 import requests
 import os
 import pathos.multiprocessing as p
+import pprint
+from geocoding import Geocoder
 
 class EstTimePipeline:
+
     def __init__(self):
         self.url = "http://datamall2.mytransport.sg/ltaodataservice/EstTravelTimes"
         self.headers = {'AccountKey' : os.environ.get("LTA_KEY"), "accept": "application/json"} 
@@ -17,12 +20,31 @@ class EstTimePipeline:
         except requests.RequestException as e:
             print(f"Error fetching data: {e}")
             return None
+    
+    def process_data(self):
+        data = self.fetch_data()
+        geo = Geocoder()
+        # pp = pprint.PrettyPrinter(indent=4, width=80, compact=False)
+
+        ''' To do: process start and end point and collect result as a dict??'''
+        return None
 
 # Checking if I manage to pull correctly:
-print(os.environ.get("LTA_KEY"))
-trying = EstTimePipeline()
-data = trying.fetch_data()
+# trying = EstTimePipeline()
+# trying.process_data()
 
-for i in data['value']:
-    print(i)
+# print(os.environ.get("LTA_KEY"))
+# data = trying.fetch_data()
+# geo = Geocoder()
+# pp = pprint.PrettyPrinter(indent=4, width=80, compact=False)
+# pp.pprint(data)
 
+# end = geo.getLocation(data['value'][0]['EndPoint'])
+
+# print('\n')
+
+# pp.pprint(data['value'][0])
+
+
+# for i in data['value']:
+#     print(i['EndPoint'])
