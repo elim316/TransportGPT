0e4556fd3bf32e1a6a728de95fe757d2d58e1cd5
jdengoh <spect13317@gmail.com>
2024-03-22 10:45:47 +0800

    knr model

--- tgpt/data/trafficFlow.py
+++ tgpt/data/trafficFlow.py
@@ -3,12 +3,20 @@ import os
 import pathos.multiprocessing as p
 import numpy as np
 import geocoder
+import pprint
+import pandas as pd
+import json
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn import model_selection
+from sklearn import metrics
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import r2_score
 
 class TrafficFlowPipeline:
     def __init__(self):
         self.url = "http://datamall2.mytransport.sg/ltaodataservice/TrafficFlow"
         self.headers = {'AccountKey': os.environ.get("LTA_KEY"), "accept": "application/json"}
-        self.n_core = p.cpu_count() // 2
+        # self.n_core = p.cpu_count() // 2
 
     def get_traffic_data_as_user(self):
         lat, long = self.get_user_lat_long()
@@ -23,19 +31,60 @@ class TrafficFlowPipeline:
             return None
 
     def get_traffic_data(self, lat, long):
+        pp = pprint.PrettyPrinter(indent=4, width=80, compact=False, sort_dicts=False)
+
         try:
             response = requests.get(self.url, headers=self.headers)
             response.raise_for_status()
-            data = response.json()
+            print("success")
+            return_link = response.json()
+            print(return_link['value'][0]['Link'])
+            data=requests.get(return_link['value'][0]['Link']).json()
+            # with open('trafficflow.json') as f:
+            #     data = json.load(f)
 
-            if data is not None and 'value' in data:
-                filtered_data = self.filter_traffic_data(data['value'], lat, long)
-                return filtered_data
-            else:
-                return None
+            trafficData = pd.json_normalize(data['Value'], meta=['LinkID', 'Date', "HourOfDate", "Volume", "StartLon", "StartLat", "EndLon", "EndLat", "RoadName", "RoadCat"])
+            print(trafficData.duplicated(subset=["RoadName"]).sum())
+            trafficData.drop_duplicates(subset=["RoadName"], inplace=True)
+            print(len(trafficData))
+            trafficData["mid_lat"] = (trafficData["StartLat"].astype(float)+trafficData["EndLat"].astype(float))
+            trafficData["mid_lon"] = (trafficData["StartLon"].astype(float)+trafficData["EndLon"].astype(float))
+            print (len(trafficData))
+            self.knr_model(trafficData)
+
+            return trafficData
+
+            # if data is not None and 'value' in data:
+            #     filtered_data = self.filter_traffic_data(data['value'], lat, long)
+            #     return filtered_data
+            # else:
+            #     return None
         except requests.RequestException as e:
             print(f"Error fetching traffic data: {e}")
             return None
+        
+    def knr_model(self, data):
+        x_train, x_test, y_train, y_test = model_selection.train_test_split(data.loc[:, ['mid_lat', 'mid_lon']], data['Volume'].astype(float), test_size=0.2, random_state=1)
+        knr = KNeighborsRegressor(n_neighbors=len(x_train),
+                                  weights='distance',  # Use distance weights
+                                  metric='euclidean')
+        scaler = StandardScaler()
+        scaler.fit(x_train)
+        x_train_scaled = scaler.transform(x_train)
+        x_test_scaled = scaler.transform(x_test)
+
+        print(y_train.info())
+        print(y_train)
+        print(x_train)
+        print(x_train_scaled)
+        print(x_test.info())
+        knr.fit(x_train_scaled,y_train)
+        x_test_pred = knr.predict(x_test_scaled)
+        print(x_test_pred)
+        print(y_test)
+        r2 = r2_score(y_test, x_test_pred)
+        print(r2)
+
 
     def filter_traffic_data(self, data, lat, long):
         # Initialize an empty list to store the filtered data
@@ -150,3 +199,4 @@ else:
 #     print("Traffic data retrieved successfully.")
 # else:
 #     print("Failed to retrieve traffic data.")
+
